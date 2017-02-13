310c52018a46049b9df7f4b260832333f8f4938f
Sujit Roy <sujitr@gmail.com>
2017-02-13 09:29:45 +0000

    adding command line runner bean for running in standalone mode

--- spring-boot-standalone/src/main/java/com/sujit/App.java
+++ spring-boot-standalone/src/main/java/com/sujit/App.java
@@ -2,7 +2,9 @@ package com.sujit;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
-
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class App {
@@ -10,5 +12,10 @@ public class App {
         SpringApplication.run(App.class, args);
     }
     
-    
+    @Bean
+    public CommandLineRunner commandLineRunner (ApplicationContext ctx) {
+       return args -> {
+            System.out.println(">>> Standalone Spring Boot application - Hello World!!!");   
+       };
+    }
 }
