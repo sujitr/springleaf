8707dcf4acf640b5519705ed9870156ac569c6cb
Sujit Roy <sujitr@gmail.com>
2017-02-13 09:23:55 +0000

    adding spring boot dependencies and re-configuring main application class

--- spring-boot-standalone/src/main/java/com/sujit/App.java
+++ spring-boot-standalone/src/main/java/com/sujit/App.java
@@ -1,13 +1,14 @@
 package com.sujit;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+
+@SpringBootApplication
+public class App {
+    public static void main( String[] args ) {
+        SpringApplication.run(App.class, args);
     }
+    
+    
 }
