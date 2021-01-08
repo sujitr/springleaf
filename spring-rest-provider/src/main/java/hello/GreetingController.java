ee5f5f2abff4bf93bcb8545926ebb094c038ccef
Sujit Roy <sujitr@gmail.com>
2021-01-08 19:50:43 +0530

    adding extended request mapping syntax

--- spring-rest-provider/src/main/java/hello/GreetingController.java
+++ spring-rest-provider/src/main/java/hello/GreetingController.java
@@ -2,6 +2,7 @@ package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class GreetingController {
     @RequestMapping("/hello")
     public String hello() {
     	return "Hello World! With DevTools.";
+    }
+    
+    @RequestMapping(method=RequestMethod.GET, path="/hello-world")
+    public String helloWorld() {
+    	return "Hello World! With DevTools and extended request mapping";
     }
 }
