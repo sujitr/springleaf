8a52c01649e600c71adb9d494367a2a01d768c44
Sujit Roy <sujitr@gmail.com>
2021-01-16 20:19:09 +0530

    Explanation of using the repository interface to eliminate most basic boilerplate steps for managing repositories and transactions

--- spring-jpa/src/main/java/com/sujit/dao/ItemRepository.java
+++ spring-jpa/src/main/java/com/sujit/dao/ItemRepository.java
@@ -3,6 +3,16 @@ package com.sujit.dao;
 import com.sujit.entity.Item;
 import org.springframework.data.repository.CrudRepository;
 
+
+/**
+ * Defining a repository interface as below helps us to avoid the 
+ * repetitive steps which are needed in general to create a DAO
+ * service like - 
+ * - declaring a repository (@Repository)
+ * - Manage it with @Transactionsal
+ * - Use an Entity Manager with @PersistenceContext etc
+ */
+
 public interface ItemRepository extends CrudRepository<Item, Long> {
     Item findById(long id);
 }
