9ba06908e1046b2b9fde99b1c641b86764191917
Sujit Roy <sujitr@gmail.com>
2021-01-16 20:23:42 +0530

    adding more explanations for making use of Repository interfaces

--- spring-jpa/src/main/java/com/sujit/dao/ItemRepository.java
+++ spring-jpa/src/main/java/com/sujit/dao/ItemRepository.java
@@ -9,8 +9,9 @@ import org.springframework.data.repository.CrudRepository;
  * repetitive steps which are needed in general to create a DAO
  * service like - 
  * - declaring a repository (@Repository)
- * - Manage it with @Transactionsal
- * - Use an Entity Manager with @PersistenceContext etc
+ * - Manage it with @Transactionsal, for declarative transaction management
+ * 		which basically says each method within the repo class would be involved in a transaction
+ * - Use an Entity Manager with @PersistenceContext for tracking changes etc
  */
 
 public interface ItemRepository extends CrudRepository<Item, Long> {
