afc51f5ff7be1f6191f6556ca35f455864662aef
Sujit Roy <sujitr@gmail.com>
2021-01-17 20:27:13 +0530

    Spring Boot REST with JPA and Hibernate. Initial Commit.

---
+++ spring-rest-web-services-jpa/src/main/java/com/sujit/restjpaweb/SpringRestWebServicesJpaApplication.java
@@ -0,0 +1,13 @@
+package com.sujit.restjpaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestWebServicesJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestWebServicesJpaApplication.class, args);
+	}
+
+}
---
+++ spring-rest-web-services-jpa/src/test/java/com/sujit/restjpaweb/SpringRestWebServicesJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujit.restjpaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestWebServicesJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
